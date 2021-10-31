338f3289cef3a95b607bc89124e0087b115c7b33
哈哈与黑洞大笑 <80620808+bigbigbig2@users.noreply.github.com>
2021-10-31 23:21:40 +0800

    最新音乐ok

--- src/views/02.playlists.vue
+++ src/views/02.playlists.vue
@@ -3,196 +3,242 @@
     <!-- 同步 -->
     <div class="top-card">
       <div class="icon-wrap">
-        <img src="../assets/listCover.jpg" alt="" />
+        <!-- 封面 -->
+        <img :src="topList.coverImgUrl" alt="" />
       </div>
       <div class="content-wrap">
         <div class="tag">精品歌单</div>
+        <!-- 标题 -->
         <div class="title">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, laborum.
+          {{ topList.name }}
         </div>
+        <!-- 介绍 -->
         <div class="info">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-          velit suscipit cumque incidunt ad vitae quos dicta temporibus, at,
-          labore harum libero. Officiis commodi illo, minus sed ad ab provident,
-          maxime nobis culpa esse iste quibusdam! Doloribus itaque quia
-          recusandae? Incidunt cumque non et minus magni dolores repudiandae
-          quia quas esse ipsam labore doloremque, maxime nobis odio ab.
-          Distinctio nemo nostrum numquam dolores labore laboriosam doloremque
-          harum vero aliquam sit et quam laborum magnam animi, quae beatae
-          fugiat sapiente! Perspiciatis libero atque ratione quam reprehenderit
-          numquam distinctio. Maxime reprehenderit, dicta placeat enim
-          laudantium voluptatem, ab eveniet nulla asperiores minima quidem!
+          {{ topList.description }}
         </div>
       </div>
-      <img src="../assets/listCover.jpg" alt="" class="bg" />
+      <!-- 背景 -->
+      <img :src="topList.coverImgUrl" alt="" class="bg" />
       <div class="bg-mask"></div>
     </div>
     <div class="tab-container">
       <!-- tab栏 顶部 -->
       <div class="tab-bar">
-        <span class="item active">全部</span>
-        <span class="item">欧美</span>
-        <span class="item">华语</span>
-        <span class="item">流行</span>
-        <span class="item">说唱</span>
-        <span class="item">摇滚</span>
-        <span class="item">民谣</span>
-        <span class="item">电子</span>
-        <span class="item">轻音乐</span>
-        <span class="item">影视原声</span>
-        <span class="item">ACG</span>
-        <span class="item">怀旧</span>
-        <span class="item">治愈</span>
-        <span class="item">旅行</span>
+        <span
+          class="item"
+          :class="{ active: tag == '全部' }"
+          @click="tag = '全部'"
+        >
+          全部
+        </span>
+        <span
+          class="item"
+          :class="{ active: tag == '欧美' }"
+          @click="tag = '欧美'"
+        >
+          欧美
+        </span>
+        <span
+          class="item"
+          :class="{ active: tag == '华语' }"
+          @click="tag = '华语'"
+        >
+          华语
+        </span>
+        <span
+          class="item"
+          :class="{ active: tag == '流行' }"
+          @click="tag = '流行'"
+        >
+          流行
+        </span>
+        <span
+          class="item"
+          :class="{ active: tag == '说唱' }"
+          @click="tag = '说唱'"
+        >
+          说唱
+        </span>
+        <span
+          class="item"
+          :class="{ active: tag == '摇滚' }"
+          @click="tag = '摇滚'"
+        >
+          摇滚
+        </span>
+        <span
+          class="item"
+          :class="{ active: tag == '民谣' }"
+          @click="tag = '民谣'"
+        >
+          民谣
+        </span>
+        <span
+          class="item"
+          :class="{ active: tag == '电子' }"
+          @click="tag = '电子'"
+        >
+          电子
+        </span>
+        <span
+          class="item"
+          :class="{ active: tag == '轻音乐' }"
+          @click="tag = '轻音乐'"
+        >
+          轻音乐
+        </span>
+        <span
+          class="item"
+          :class="{ active: tag == '影视原声' }"
+          @click="tag = '影视原声'"
+        >
+          影视原声
+        </span>
+        <span
+          class="item"
+          :class="{ active: tag == 'ACG' }"
+          @click="tag = 'ACG'"
+        >
+          ACG
+        </span>
+        <span
+          class="item"
+          :class="{ active: tag == '怀旧' }"
+          @click="tag = '怀旧'"
+        >
+          怀旧
+        </span>
+        <span
+          class="item"
+          :class="{ active: tag == '治愈' }"
+          @click="tag = '治愈'"
+        >
+          治愈
+        </span>
+        <span
+          class="item"
+          :class="{ active: tag == '旅行' }"
+          @click="tag = '旅行'"
+        >
+          旅行
+        </span>
       </div>
       <!-- tab的内容区域 -->
       <div class="tab-content">
         <div class="items">
-          <div class="item">
+          <div class="item" v-for="(item, index) in list" :key="index">
             <div class="img-wrap">
               <div class="num-wrap">
                 播放量:
-                <span class="num">66892</span>
+                <span class="num">{{ item.playCount }}</span>
               </div>
-              <img src="../assets/cover.jpg" alt="" />
+              <img :src="item.coverImgUrl" alt="" />
               <span class="iconfont icon-play"></span>
             </div>
-            <p class="name">编辑推荐：一起探索这个未知的音乐罐头吧！</p>
-          </div>
-          <div class="item">
-            <div class="img-wrap">
-              <div class="num-wrap">
-                播放量:
-                <span class="num">66892</span>
-              </div>
-              <img src="../assets/cover.jpg" alt="" />
-              <span class="iconfont icon-play"></span>
-            </div>
-            <p class="name">编辑推荐：一起探索这个未知的音乐罐头吧！</p>
-          </div>
-          <div class="item">
-            <div class="img-wrap">
-              <div class="num-wrap">
-                播放量:
-                <span class="num">66892</span>
-              </div>
-              <img src="../assets/cover.jpg" alt="" />
-              <span class="iconfont icon-play"></span>
-            </div>
-            <p class="name">编辑推荐：一起探索这个未知的音乐罐头吧！</p>
-          </div>
-          <div class="item">
-            <div class="img-wrap">
-              <div class="num-wrap">
-                播放量:
-                <span class="num">66892</span>
-              </div>
-              <img src="../assets/cover.jpg" alt="" />
-              <span class="iconfont icon-play"></span>
-            </div>
-            <p class="name">编辑推荐：一起探索这个未知的音乐罐头吧！</p>
-          </div>
-          <div class="item">
-            <div class="img-wrap">
-              <div class="num-wrap">
-                播放量:
-                <span class="num">66892</span>
-              </div>
-              <img src="../assets/cover.jpg" alt="" />
-              <span class="iconfont icon-play"></span>
-            </div>
-            <p class="name">编辑推荐：一起探索这个未知的音乐罐头吧！</p>
-          </div>
-          <div class="item">
-            <div class="img-wrap">
-              <div class="num-wrap">
-                播放量:
-                <span class="num">66892</span>
-              </div>
-              <img src="../assets/cover.jpg" alt="" />
-              <span class="iconfont icon-play"></span>
-            </div>
-            <p class="name">编辑推荐：一起探索这个未知的音乐罐头吧！</p>
-          </div>
-          <div class="item">
-            <div class="img-wrap">
-              <div class="num-wrap">
-                播放量:
-                <span class="num">66892</span>
-              </div>
-              <img src="../assets/cover.jpg" alt="" />
-              <span class="iconfont icon-play"></span>
-            </div>
-            <p class="name">编辑推荐：一起探索这个未知的音乐罐头吧！</p>
-          </div>
-          <div class="item">
-            <div class="img-wrap">
-              <div class="num-wrap">
-                播放量:
-                <span class="num">66892</span>
-              </div>
-              <img src="../assets/cover.jpg" alt="" />
-              <span class="iconfont icon-play"></span>
-            </div>
-            <p class="name">编辑推荐：一起探索这个未知的音乐罐头吧！</p>
-          </div>
-          <div class="item">
-            <div class="img-wrap">
-              <div class="num-wrap">
-                播放量:
-                <span class="num">66892</span>
-              </div>
-              <img src="../assets/cover.jpg" alt="" />
-              <span class="iconfont icon-play"></span>
-            </div>
-            <p class="name">编辑推荐：一起探索这个未知的音乐罐头吧！</p>
-          </div>
-          <div class="item">
-            <div class="img-wrap">
-              <div class="num-wrap">
-                播放量:
-                <span class="num">66892</span>
-              </div>
-              <img src="../assets/cover.jpg" alt="" />
-              <span class="iconfont icon-play"></span>
-            </div>
-            <p class="name">编辑推荐：一起探索这个未知的音乐罐头吧！</p>
+            <p class="name">{{ item.name }}</p>
           </div>
         </div>
       </div>
     </div>
-    <!-- 分页器 -->
+    <!-- 分页器
+      total 总条数
+      current-page 当前页
+      page-size 每页多少条数据
+      current-change 页码改变事件
+     -->
     <el-pagination
       @current-change="handleCurrentChange"
       background
       layout="prev, pager, next"
       :total="total"
       :current-page="page"
-      :page-size="20"
-    >
-    </el-pagination>
+      :page-size="10"
+    ></el-pagination>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'recommend',
-  data() {
-    return {
-      // 总条数
-      total:0,
-      // 页码
-      page:1
-    };
-  },
-  methods: {
-    handleCurrentChange(val) {
-      console.log(`当前页: ${val}`);
+  // 导入 axios
+  import axios from 'axios'
+  export default {
+    name: 'recommend',
+    data() {
+      return {
+        // 总条数
+        total: 0,
+        // 页码
+        page: 1,
+        // 顶部的推荐歌单
+        topList: {},
+        // 歌单列表
+        list: [],
+        // 当前选择的数据
+        tag: '全部'
+      }
+    },
+    // 侦听器
+    watch: {
+      tag() {
+        // 顶部的 精品歌单
+        this.topData()
+        // 歌单列表
+        this.listData()
+        // 修改页码为1
+        this.page = 1
+      }
+    },
+    created() {
+      // 顶部的 精品歌单
+      this.topData()
+      // 歌单列表
+      this.listData()
+    },
+    methods: {
+      // 抽取的方法1 顶部的数据
+      topData() {
+        axios({
+          url: 'https://autumnfish.cn/top/playlist/highquality',
+          method: 'get',
+          params: {
+            limit: 1,
+            // 分类数据
+            cat: this.tag
+          }
+        }).then(res => {
+          // console.log(res)
+          this.topList = res.data.playlists[0]
+        })
+      },
+      // 抽取的方法2 列表数据
+      listData() {
+        axios({
+          url: 'https://autumnfish.cn/top/playlist/',
+          method: 'get',
+          params: {
+            limit: 10,
+            // 起始的值 （页码-1）*每页多少条数据
+            offset: (this.page - 1) * 10,
+            // 分类数据
+            cat: this.tag
+          }
+        }).then(res => {
+          // console.log(res)
+          // 保存总条数
+          this.total = res.data.total
+          // 保存数据
+          this.list = res.data.playlists
+        })
+      },
+      // 页码改变事件
+      handleCurrentChange(val) {
+        // console.log(`当前页: ${val}`)
+        // 保存页码
+        this.page = val
+        // 重新获取数据
+        this.listData()
+      }
     }
   }
-};
 </script>
 
-<style >
-
-</style>
+<style></style>
--- src/views/03.songs.vue
+++ src/views/03.songs.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="songs-container">
+    <!--最新歌曲分类-->
     <div class="tab-bar">
-      <span class="item active">全部</span>
-      <span class="item">华语</span>
-      <span class="item">欧美</span>
-      <span class="item">日本</span>
-      <span class="item">韩国</span>
+      <span class="item" @click="tag = 0" :class="{ active: tag == 0 }">
+        全部
+      </span>
+      <span class="item" @click="tag = 7" :class="{ active: tag == 7 }">
+        华语
+      </span>
+      <span class="item" @click="tag = 96" :class="{ active: tag == 96 }">
+        欧美
+      </span>
+      <span class="item" @click="tag = 8" :class="{ active: tag == 8 }">
+        日本
+      </span>
+      <span class="item" @click="tag = 16" :class="{ active: tag == 16 }">
+        韩国
+      </span>
     </div>
-    <!-- 底部的table -->
+    <!-- 歌曲列表及顶部的title -->
     <table class="el-table playlit-table">
       <thead>
         <th></th>
@@ -18,46 +29,27 @@
         <th>时长</th>
       </thead>
       <tbody>
-        <tr class="el-table__row">
-          <td>1</td>
+        <tr v-for="(item, index) in lists" :key="index" class="el-table__row">
+          <td>{{ index + 1 }}</td>
           <td>
             <div class="img-wrap">
-              <img src="../assets/songCover.jpg" alt="" />
-              <span class="iconfont icon-play"></span>
+              <img :src="item.album.picUrl" alt="" />
+              <!-- 播放按钮 -->
+              <span @click="playMusic(item.id)" class="iconfont icon-play"></span>
             </div>
           </td>
           <td>
             <div class="song-wrap">
               <div class="name-wrap">
-                <span>你要相信这不是最后一天</span>
+                <span>{{ item.name }}</span>
                 <span class="iconfont icon-mv"></span>
               </div>
-              <span>电视剧加油练习生插曲</span>
+              <span></span>
             </div>
           </td>
-          <td>华晨宇</td>
-          <td>你要相信这不是最后一天</td>
-          <td>06:03</td>
-        </tr>
-        <tr class="el-table__row">
-          <td>2</td>
-          <td>
-            <div class="img-wrap">
-              <img src="../assets/songCover.jpg" alt="" />
-              <span class="iconfont icon-play"></span>
-            </div>
-          </td>
-          <td>
-            <div class="song-wrap">
-              <div class="name-wrap">
-                <span>你要相信这不是最后一天</span>
-                <span class="iconfont icon-mv"></span>
-              </div>
-            </div>
-          </td>
-          <td>华晨宇</td>
-          <td>你要相信这不是最后一天</td>
-          <td>06:03</td>
+          <td>{{ item.album.artists['0'].name }}</td>
+          <td>{{ item.album.name }}</td>
+          <td>{{ item.duration }}</td>
         </tr>
       </tbody>
     </table>
@@ -65,16 +57,69 @@
 </template>
 
 <script>
-export default {
-  name: 'songs',
-  data() {
-    return {
- 
-    };
+  // 导入 axios
+  import axios from 'axios'
+  export default {
+    name: 'songs',
+    data() {
+      return {
+        // 歌曲列表
+        lists: [],
+        // 分类
+        tag: '0'
+      }
+    },
+    watch: {
+      tag() {
+        this.getList()
+      }
+    },
+    created() {
+      this.getList()
+    },
+    methods: {
+      // 获取列表数据
+      getList() {
+        // 获取 最新音乐数据
+        axios({
+          url: 'https://autumnfish.cn/top/song',
+          method: 'get',
+          params: {
+            // 传递 设置的数据
+            type: this.tag
+          }
+        }).then(res => {
+          // console.log(res)
+          this.lists = res.data.data
+          // 处理时长 毫秒 转为 分秒
+          for (let i = 0; i < this.lists.length; i++) {
+            // 获取 总毫秒数
+            let duration = this.lists[i].duration
+            // 假定 	350750 毫秒
+            // 秒 350750/1000  350秒
+            // 分 350 /60
+            // 秒 350 % 60
+            let min = parseInt(duration / 1000 / 60)
+            if (min < 10) {
+              min = '0' + min
+            }
+            let sec = parseInt((duration / 1000) % 60)
+            if (sec < 10) {
+              sec = '0' + sec
+            }
+            // console.log(min + '|' + sec)
+            this.lists[i].duration = `${min}:${sec}`
+          }
+        })
+      },
+      // 播放歌曲
+      playMusic(id){
+        let url = "https://music.163.com/song/media/outer/url?id=" + id + ".mp3"
+        //将url传递给这个值父组件mari.vue
+        this.$parent.musicUrl = url
+      }
+    }
   }
-};
 </script>
 
-<style >
-
-</style>
+<style></style>
